2820ac66d994579e9a111026d84d1567c35f8cea
WmW <zhouzheng0646@qq.com>
2023-12-16 15:50:01 +0800

    delete app

--- app-ui/src/server/index.js
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import axios from "axios"
-
-export async function fetchIndex() {
-  let ret = await axios.get("/")
-  console.log("【 ret 】==>", ret);
-  return "the index"
-}
--- app-ui/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-// utils/api.js
-import axios from 'axios';
-
-export function initAxios() {
-  axios.defaults.baseURL = 'https://qbot.ai:8080';
-  axios.defaults.headers = { 'Content-Type': 'application/json' }
-}
